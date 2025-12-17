ab3019cbaa88546ccb935b533864d74fe0e9f33f
Muhammed Nihal <nihalk57p@gmail.com>
2025-12-17 13:44:30 +0400

    impliment headder

---
+++ components/Header.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import Image from "next/image";
+import NavItems from "@/components/NavItems";
+import UserDropdown from "@/components/UserDropdown";
+
+const Header = () => {
+    return (
+        <header className="sticky top-0 header">
+            <div className="container header-wrapper">
+
+                {/* Logo */}
+                <Link href="/">
+                    <Image
+                        src="/assets/icons/logo.svg"
+                        alt="Signalist logo"
+                        width={140}
+                        height={32}
+
+                    />
+                </Link>
+
+                {/* Navigation */}
+                <nav className="hidden sm:block">
+                    <NavItems/>
+                </nav>
+
+                <UserDropdown/>
+
+            </div>
+        </header>
+    );
+};
+
+export default Header;
